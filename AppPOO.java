30ca8b0bc57de23e0fa7881d40741216bb115ee8
unknown <JhonRodriguez@rfast.local>
2025-05-24 15:28:57 -0500

    Agregar polimorfismo con clases Hombre y Mujer

--- AppPOO.java
+++ AppPOO.java
@@ -1,92 +1,101 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 class Persona {
-    // a) Propiedades privadas
-    private String nombre;
-    private String apellido;
-    private String genero;
-    private int edad;
+    protected String nombre;
+    protected int edad;
 
-    // Constructor
-    public Persona(String nombre, String apellido, String genero, int edad) {
+    public Persona(String nombre, int edad) {
         this.nombre = nombre;
-        this.apellido = apellido;
-        this.genero = genero;
         this.edad = edad;
-    }
-
-    // Métodos getters para acceder a los atributos necesarios
-    public String getNombre() {
-        return nombre;
-    }
-
-    public String getGenero() {
-        return genero;
     }
 
     public int getEdad() {
         return edad;
     }
+
+    public String getGenero() {
+        return "Sin especificar";
+    }
+
+    public String getInfo() {
+        return "Nombre: " + nombre + ", Género: " + getGenero();
+    }
+}
+
+class Hombre extends Persona {
+    public Hombre(String nombre, int edad) {
+        super(nombre, edad);
+    }
+
+    @Override
+    public String getGenero() {
+        return "Masculino";
+    }
+
+    @Override
+    public String getInfo() {
+        return "HOMBRE - " + super.getInfo();
+    }
+}
+
+class Mujer extends Persona {
+    public Mujer(String nombre, int edad) {
+        super(nombre, edad);
+    }
+
+    @Override
+    public String getGenero() {
+        return "Femenino";
+    }
+
+    @Override
+    public String getInfo() {
+        return "MUJER - " + super.getInfo();
+    }
 }
 
 public class AppPOO {
-    static ArrayList<Persona> personas = new ArrayList<>();
-    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        capturarPersonas();
-        mostrarNombreYGenero();
-        System.out.println("Promedio de edades: " + calcularPromedioEdad());
-        System.out.println("Cantidad de personas con género Masculino: " + contarGenero("Masculino"));
-        System.out.println("Cantidad de personas con género Femenino: " + contarGenero("Femenino"));
-    }
+        Scanner scanner = new Scanner(System.in);
+        Persona[] personas = new Persona[5];
+        int totalEdad = 0;
+        int countHombres = 0;
+        int countMujeres = 0;
 
-    // b) Método para capturar 5 personas por teclado
-    public static void capturarPersonas() {
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Ingrese los datos de la persona #" + (i + 1));
-            System.out.print("Nombre: ");
+        for (int i = 0; i < personas.length; i++) {
+            System.out.println("Ingrese el nombre de la persona #" + (i + 1) + ": ");
             String nombre = scanner.nextLine();
 
-            System.out.print("Apellido: ");
-            String apellido = scanner.nextLine();
-
-            System.out.print("Género (Masculino/Femenino): ");
-            String genero = scanner.nextLine();
-
-            System.out.print("Edad: ");
+            System.out.println("Ingrese la edad de " + nombre + ": ");
             int edad = Integer.parseInt(scanner.nextLine());
 
-            personas.add(new Persona(nombre, apellido, genero, edad));
-        }
-    }
+            System.out.println("Ingrese el género (Masculino/Femenino): ");
+            String genero = scanner.nextLine().trim().toLowerCase();
 
-    // b) Método para mostrar nombre y género
-    public static void mostrarNombreYGenero() {
-        System.out.println("\n--- Nombre y Género de las personas ---");
-        for (Persona p : personas) {
-            System.out.println("Nombre: " + p.getNombre() + " | Género: " + p.getGenero());
-        }
-    }
-
-    // c) Promedio de edad
-    public static double calcularPromedioEdad() {
-        int suma = 0;
-        for (Persona p : personas) {
-            suma += p.getEdad();
-        }
-        return (double) suma / personas.size();
-    }
-
-    // d) Contar personas por género
-    public static int contarGenero(String generoBuscado) {
-        int contador = 0;
-        for (Persona p : personas) {
-            if (p.getGenero().equalsIgnoreCase(generoBuscado)) {
-                contador++;
+            if (genero.equals("masculino")) {
+                personas[i] = new Hombre(nombre, edad);
+                countHombres++;
+            } else if (genero.equals("femenino")) {
+                personas[i] = new Mujer(nombre, edad);
+                countMujeres++;
+            } else {
+                System.out.println("Género no válido. Se asignará como Hombre por defecto.");
+                personas[i] = new Hombre(nombre, edad);
+                countHombres++;
             }
+
+            totalEdad += edad;
         }
-        return contador;
+
+        System.out.println("\n--- Información de las personas capturadas ---");
+        for (Persona p : personas) {
+            System.out.println(p.getInfo());
+        }
+
+        double promedioEdad = (double) totalEdad / personas.length;
+        System.out.println("\nPromedio de edad: " + promedioEdad);
+        System.out.println("Cantidad de personas de género masculino: " + countHombres);
+        System.out.println("Cantidad de personas de género femenino: " + countMujeres);
     }
 }
